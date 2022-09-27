d8c383fd3bb09d65c9305e87b6390379eb91cf7c
johannkrugell <johannkrugell@gmail.com>
2022-09-28 06:17:50 +1000

    REFACTOR IF STATEMENT Refactored the if statement to complete the check if a swap is needed and that the iteration is not at the last element in the array into one if statement. Removed the nested if. Added comments to identify the step where the element swap takes place.
    On branch refactor
    Changes to be committed:
    modified:   bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,23 @@
 test_array = [4,3,78,2,0,2]
-#4,3,78,2,0,2
-#1,2,3,4,5,6
 
 def bubble_sort(array_to_sort)
+  # set loop counter
   i = 0
-  while i < array_to_sort.length ** 2 do array_to_sort.each_with_index { |number, index|       
-    if index+1 < array_to_sort.length 
-        var1 = array_to_sort[index]
-        var2 = array_to_sort[index+1]
-    if number > array_to_sort[index+1] && index+1 < array_to_sort.length
-        array_to_sort[index]= var2
-        array_to_sort[index+1] = var1  
-        array_to_sort      
-    elsif
-      number < array_to_sort[index+1] && index+1 < array_to_sort.length
-      number = number
-    else
-       number = number
-    end
-    else
-      array_to_sort
-    end 
-  
-    i += 1
-  }
+  while i < array_to_sort.length ** 2 do 
+    array_to_sort.each_with_index { |number, index|       
+      if index+1 < array_to_sort.length && number > array_to_sort[index+1]    
+        # get current and next element values
+        current_element = array_to_sort[index]
+        next_element = array_to_sort[index+1]
+        # swap elements  
+        array_to_sort[index]= next_element
+        array_to_sort[index+1] = current_element    
+      else
+        number = current_element
+      end 
+        #increment loop counter
+        i += 1
+    }
   end
   return array_to_sort
 end
